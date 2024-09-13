2e272858beb318b319f74bb1ff5c0e8b4a015ce5
mercury-paws <145927427+mercury-paws@users.noreply.github.com>
2024-09-13 11:44:48 +0200

    notfoundpage

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,10 +1,26 @@
+import Logo from "../Logo/Logo";
 import css from "./NotFound.module.css";
+import clsx from "clsx";
 
 function NotFound() {
   return (
-    <>
-      <p className={css.notFound}>Not Found</p>
-    </>
+    <div className={css.notFoundContainer}>
+      <div className={css.notFound}>
+        <div className={css.logo}>
+          <Logo />
+        </div>
+        <h3 className={css.notFoundHeader}>
+          404
+          <br />
+          Page not found. The page you are looking for doesn't exist.
+        </h3>
+        <div>
+          <div className={clsx(css.circle, css.scaleUpDownCenter)}></div>
+          <div className={clsx(css.circle, css.scaleUpDownCenter)}></div>
+          <div className={clsx(css.circle, css.scaleUpDownCenter)}></div>
+        </div>
+      </div>
+    </div>
   );
 }
 
